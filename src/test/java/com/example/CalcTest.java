88347ed7bc0b8430559cddc8035a0c1bb04dcecc
Avril <winnie2002824@gmail.com>
2025-10-07 21:44:30 +0800

    Initial CalcTDD project with Maven and JUnit tests

---
+++ src/test/java/com/example/CalcTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class CalcTest
+{
+   @Test
+   public void testAdd()
+   {
+      assertTrue ("Calc sum incorrect", 5 == Calc.add (2, 3));
+   }
+}
